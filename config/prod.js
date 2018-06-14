ad134e9875ed59fb7222e91db12facce7b95cfa1
Roi ben haim <roi.benhaim@gmail.com>
2018-06-14 19:09:34 +0300

    no message

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+module.exports.Config = {
+
+    db: {
+        user: 'postgres',
+        host: 'ec2-54-247-111-19.eu-west-1.compute.amazonaws.com',
+        database: 'dfnpkh1mba6hcb',
+        password: '1234',
+        port: 5432,
+        url: 'postgres://gqvdtrxzfupdcq:55c742211e01f60a9efe0b25224442fe486dbd6634c508fbfb8908a13a47170e@ec2-54-247-111-19.eu-west-1.compute.amazonaws.com:5432/dfnpkh1mba6hcb'
+    }
+
+
+
+}
