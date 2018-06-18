638ff28b20dfc01aeaf4646cbeb968432de28e8a
Roi ben haim <roi.benhaim@gmail.com>
2018-06-18 19:07:05 +0300

    no message

--- content/pipe_functions.js
+++ content/pipe_functions.js
@@ -1,32 +1,31 @@
-var embeder;
+var SkriboResult;
 
 
-class Skribo {
-    ForAccounts(cb) {
-        var accountSelector = MccApp.accounts();
-        var accountIterator = accountSelector.get();
 
-        // Iterate through the list of accounts
-        while (accountIterator.hasNext()) {
-            var account = accountIterator.next();
-            Logger.log(account);
-            // Select the client account.
-            MccApp.select(account);
-            cb(account);
-        }
-    }
+function SkriboForAccounts(cb) {
+    var accountSelector = MccApp.accounts();
+    var accountIterator = accountSelector.get();
 
-
-    PostResults(results) {
-        const fetchAddr = embeder.replace('/serve/', '/results/');
-        var options = {
-            "method": "post",
-            "payload": {
-                "results": results,
-                "variables": null
-
-            }
-        };
-        UrlFetchApp.fetch(fetchAddr, options);
+    // Iterate through the list of accounts
+    while (accountIterator.hasNext()) {
+        var account = accountIterator.next();
+        Logger.log(account);
+        // Select the client account.
+        MccApp.select(account);
+        cb(account);
     }
 }
+
+
+function SkriboPostResults(results) {
+    var options = {
+        "method": "post",
+        "payload": {
+            "results": results,
+            "variables": null
+
+        }
+    };
+    UrlFetchApp.fetch(SkriboResult, options);
+}
+
