f1adfb57446e22d39013d2676509ed26a184a6cc
Roi ben haim <roi.benhaim@gmail.com>
2018-08-24 22:20:22 +0300

    no message

--- skribo.sql
+++ skribo.sql
@@ -1,3 +1,38 @@
+
+
+-- SEQUENCE: public."embeds_ID_seq"
+
+-- DROP SEQUENCE public."embeds_ID_seq";
+
+CREATE SEQUENCE public."embeds_ID_seq";
+
+ALTER SEQUENCE public."embeds_ID_seq"
+    OWNER TO postgres;
+    
+    
+    
+    -- Table: public.embeds
+
+-- DROP TABLE public.embeds;
+
+CREATE TABLE public.embeds
+(
+    "ID" integer NOT NULL DEFAULT nextval('"embeds_ID_seq"'::regclass),
+    "GroupId" character varying COLLATE pg_catalog."default",
+    "ScriptId" character varying COLLATE pg_catalog."default",
+    "Variables" character varying COLLATE pg_catalog."default",
+    "EmbedId" character varying COLLATE pg_catalog."default",
+    "Name" character varying COLLATE pg_catalog."default",
+    CONSTRAINT embeds_pkey PRIMARY KEY ("ID")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.embeds
+    OWNER to postgres;
+    
 -- SEQUENCE: public."user_groups_ID_seq"
 
 -- DROP SEQUENCE public."user_groups_ID_seq";
