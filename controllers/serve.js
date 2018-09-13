e76e81d4d3bee64b9b743051eebd8b41f657e988
Roi ben haim <roi.benhaim@gmail.com>
2018-09-14 00:52:35 +0300

    no message

--- controllers/serve.js
+++ controllers/serve.js
@@ -1,12 +1,4 @@
 "use strict";
-/*
-
-____ _  _ ____ _ ___  ____
-[__  |_/  |__/ | |__] |  |
-___] | \_ |  \ | |__] |__|
-                           
-
-*/
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -28,56 +20,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("@methodus/server");
-const script_model_1 = require("../models/script.model");
-const embed_model_1 = require("../models/embed.model");
-const FS = require("fs");
+/*
+
+____ _  _ ____ _ ___  ____
+[__  |_/  |__/ | |__] |  |
+___] | \_ |  \ | |__] |__|
+
+*/
 const data_1 = require("@methodus/data");
-const uuidv1 = require('uuid/v1');
+const server_1 = require("@methodus/server");
+const FS = require("fs");
+const embed_model_1 = require("../models/embed.model");
+const script_model_1 = require("../models/script.model");
 let Serve = class Serve {
-    static timespanToRange(timespan) {
-        //'TODAY', 'YESTERDAY', 'LAST_7_DAYS', 'THIS_WEEK_SUN_TODAY',
-        // 'THIS_WEEK_MON_TODAY', 'LAST_WEEK', 'LAST_14_DAYS', 'LAST_30_DAYS',
-        // 'LAST_WEEK', 'LAST_BUSINESS_WEEK', 'LAST_WEEK_SUN_SAT', 'THIS_MONTH', 'LAST_MONTH', 'ALL_TIME'
-        switch (timespan) {
-            case 'TODAY': {
-                let dateString = new Date().toISOString().split('T')[0];
-                return { start: dateString, end: dateString };
-            }
-            case 'YESTERDAY': {
-                let date = new Date();
-                date.setDate(date.getDate() - 1);
-                let dateString = date.toISOString().split('T')[0];
-                return { start: dateString, end: new Date().toISOString().split('T')[0] };
-            }
-            case 'LAST_7_DAYS': {
-                let date = new Date();
-                date.setDate(date.getDate() - 7);
-                let dateString = date.toISOString().split('T')[0];
-                return { start: dateString, end: new Date().toISOString().split('T')[0] };
-            }
-            case 'LAST_MONTH':
-                {
-                    let date = new Date();
-                    date.setDate(date.getDate() - 30);
-                    let dateString = date.toISOString().split('T')[0];
-                    return { start: dateString, end: new Date().toISOString().split('T')[0] };
+    static get(scriptId, groupId, embedId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const codeResult = yield script_model_1.ScriptModel.query(new data_1.Query(script_model_1.ScriptModel).filter({ ScriptId: scriptId }));
+            if (codeResult.length > 0) {
+                const InstanceScript = (yield new data_1.Query(embed_model_1.EmbedModel).filter({
+                    EmbedId: embedId, GroupId: groupId, ScriptId: scriptId,
+                }).run());
+                if (InstanceScript.length > 0) {
+                    let variables = InstanceScript[0].Variables || [];
+                    if (typeof variables === 'string') {
+                        variables = JSON.parse(variables);
+                    }
+                    const preCode = this.generateVariables(variables);
+                    const code = codeResult[0].Code;
+                    const functionCode = this.processTemplate(scriptId, groupId, embedId);
+                    return new server_1.MethodResult(preCode + functionCode + code);
                 }
-        }
-    }
-    static processTemplate(script_id, group_id, embed_id) {
-        let function_code = FS.readFileSync('./content/pipe_functions.js', { encoding: 'utf-8' });
-        const dataUrl = script_id + '/' + group_id + '/' + embed_id;
-        function_code = function_code.replace(/\$SCRIPTURL\$/g, `serve/${dataUrl}`);
-        function_code = function_code.replace(/\$LOGURL\$/g, `log/${dataUrl}`);
-        function_code = function_code.replace(/\$RESULTURL\$/g, `results/${dataUrl}/`);
-        function_code = function_code.replace(/\$SERVERURL\$/g, `https://skribo.herokuapp.com/`);
-        function_code = function_code.replace(/\$SYNCURL\$/g, `sync/${group_id}/`);
-        function_code = function_code.replace(/\$SKRIBODATA\$/g, `'` + JSON.stringify({
-            'group_id': group_id,
-            'base_url': 'https://skribo.herokuapp.com'
-        }) + `'`);
-        return function_code;
+            }
+            throw (new server_1.MethodError('not found', 404));
+        });
     }
     static generateVariables(variables) {
         return [
@@ -94,38 +69,59 @@ let Serve = class Serve {
                         return `"${item.name}":${JSON.stringify(this.timespanToRange(item.value))},`;
                 }
             }),
-            '};'
+            '};',
         ].join('\n');
     }
-    static get(script_id, group_id, embed_id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                const codeResult = yield script_model_1.ScriptModel.query(new data_1.Query(script_model_1.ScriptModel).filter({ 'ScriptId': script_id }));
-                if (codeResult.length > 0) {
-                    let code = codeResult[0].Code;
-                    const InstanceScript = (yield new data_1.Query(embed_model_1.EmbedModel).filter({ GroupId: group_id, ScriptId: script_id, EmbedId: embed_id }).run());
-                    if (InstanceScript.length > 0) {
-                        let variables = InstanceScript[0].Variables || [];
-                        if (typeof variables === 'string') {
-                            variables = JSON.parse(variables);
-                        }
-                        let preCode = this.generateVariables(variables);
-                        let function_code = this.processTemplate(script_id, group_id, embed_id);
-                        console.log('complete script', preCode);
-                        return new server_1.MethodResult(preCode + function_code + code);
-                    }
+    static timespanToRange(timespan) {
+        // 'TODAY', 'YESTERDAY', 'LAST_7_DAYS', 'THIS_WEEK_SUN_TODAY',
+        // 'THIS_WEEK_MON_TODAY', 'LAST_WEEK', 'LAST_14_DAYS', 'LAST_30_DAYS',
+        // 'LAST_WEEK', 'LAST_BUSINESS_WEEK', 'LAST_WEEK_SUN_SAT', 'THIS_MONTH', 'LAST_MONTH', 'ALL_TIME'
+        switch (timespan) {
+            case 'TODAY': {
+                const dateString = new Date().toISOString().split('T')[0];
+                return { start: dateString, end: dateString };
+            }
+            case 'YESTERDAY': {
+                const date = new Date();
+                date.setDate(date.getDate() - 1);
+                const dateString = date.toISOString().split('T')[0];
+                return { start: dateString, end: new Date().toISOString().split('T')[0] };
+            }
+            case 'LAST_7_DAYS': {
+                const date = new Date();
+                date.setDate(date.getDate() - 7);
+                const dateString = date.toISOString().split('T')[0];
+                return { start: dateString, end: new Date().toISOString().split('T')[0] };
+            }
+            case 'LAST_MONTH':
+                {
+                    const date = new Date();
+                    date.setDate(date.getDate() - 30);
+                    const dateString = date.toISOString().split('T')[0];
+                    return { start: dateString, end: new Date().toISOString().split('T')[0] };
                 }
-                throw (new server_1.MethodError('not found', 404));
-            }
-            catch (error) {
-                console.error(error);
-            }
-        });
+        }
+    }
+    static processTemplate(scriptId, groupId, embedId) {
+        let functionCode = FS.readFileSync('./content/pipe_functions.js', { encoding: 'utf-8' });
+        const dataUrl = scriptId + '/' + groupId + '/' + embedId;
+        functionCode = functionCode.replace(/\$SCRIPTURL\$/g, `serve/${dataUrl}`);
+        functionCode = functionCode.replace(/\$LOGURL\$/g, `log/${dataUrl}`);
+        functionCode = functionCode.replace(/\$RESULTURL\$/g, `results/${dataUrl}/`);
+        functionCode = functionCode.replace(/\$SERVERURL\$/g, `https://skribo.herokuapp.com/`);
+        functionCode = functionCode.replace(/\$SYNCURL\$/g, `sync/${groupId}/`);
+        functionCode = functionCode.replace(/\$SKRIBODATA\$/g, `'` + JSON.stringify({
+            base_url: 'https://skribo.herokuapp.com',
+            group_id: groupId,
+        }) + `'`);
+        return functionCode;
     }
 };
 __decorate([
     server_1.Method("GET" /* Get */, '/serve/:script_id/:group_id/:embed_id'),
-    __param(0, server_1.Param('script_id')), __param(1, server_1.Param("group_id")), __param(2, server_1.Param('embed_id')),
+    __param(0, server_1.Param('script_id')),
+    __param(1, server_1.Param('group_id')),
+    __param(2, server_1.Param('embed_id')),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [String, String, String]),
     __metadata("design:returntype", Promise)
--- controllers/serve.ts
+++ controllers/serve.ts
@@ -1,77 +1,45 @@
 /*
 
-____ _  _ ____ _ ___  ____ 
-[__  |_/  |__/ | |__] |  | 
-___] | \_ |  \ | |__] |__| 
-                           
+____ _  _ ____ _ ___  ____
+[__  |_/  |__/ | |__] |  |
+___] | \_ |  \ | |__] |__|
 
 */
-
-import { Method, MethodConfig, Param, Verbs, MethodError, MethodResult } from '@methodus/server';
-
-import { ScriptModel } from '../models/script.model';
-import { EmbedModel } from '../models/embed.model';
+import { Query as DataQuery } from '@methodus/data';
+import { Method, MethodConfig, MethodError, MethodResult, Param, Verbs } from '@methodus/server';
 import * as FS from 'fs';
-import { Repo, Query as DataQuery } from '@methodus/data';
-const uuidv1 = require('uuid/v1');
-
+import { AutoLogger } from 'logelas';
+import { EmbedModel } from '../models/embed.model';
+import { ScriptModel } from '../models/script.model';
 @MethodConfig('Serve')
 export class Serve {
 
+    @Method(Verbs.Get, '/serve/:script_id/:group_id/:embed_id')
+    public static async get(
+        @Param('script_id') scriptId: string,
+        @Param('group_id') groupId: string,
+        @Param('embed_id') embedId: string): Promise<MethodResult<string>> {
+        const codeResult = await ScriptModel.query(new DataQuery(ScriptModel).filter({ ScriptId: scriptId }));
+        if (codeResult.length > 0) {
 
-    static timespanToRange(timespan) {
-        //'TODAY', 'YESTERDAY', 'LAST_7_DAYS', 'THIS_WEEK_SUN_TODAY',
-        // 'THIS_WEEK_MON_TODAY', 'LAST_WEEK', 'LAST_14_DAYS', 'LAST_30_DAYS',
-        // 'LAST_WEEK', 'LAST_BUSINESS_WEEK', 'LAST_WEEK_SUN_SAT', 'THIS_MONTH', 'LAST_MONTH', 'ALL_TIME'
-        switch (timespan) {
-            case 'TODAY': {
-                let dateString = new Date().toISOString().split('T')[0];
-                return { start: dateString, end: dateString }
-            }
-
-            case 'YESTERDAY': {
-                let date = new Date();
-                date.setDate(date.getDate() - 1);
-                let dateString = date.toISOString().split('T')[0];
-                return { start: dateString, end: new Date().toISOString().split('T')[0] }
-            }
-
-            case 'LAST_7_DAYS': {
-                let date = new Date();
-                date.setDate(date.getDate() - 7);
-                let dateString = date.toISOString().split('T')[0];
-                return { start: dateString, end: new Date().toISOString().split('T')[0] }
-            }
-
-            case 'LAST_MONTH':
-                {
-                    let date = new Date();
-                    date.setDate(date.getDate() - 30);
-                    let dateString = date.toISOString().split('T')[0];
-                    return { start: dateString, end: new Date().toISOString().split('T')[0] }
+            const InstanceScript = (await new DataQuery(EmbedModel).filter({
+                EmbedId: embedId, GroupId: groupId, ScriptId: scriptId,
+            }).run());
+            if (InstanceScript.length > 0) {
+                let variables = InstanceScript[0].Variables || [];
+                if (typeof variables === 'string') {
+                    variables = JSON.parse(variables);
                 }
-
-
+                const preCode = this.generateVariables(variables);
+                const code = codeResult[0].Code;
+                const functionCode: string = this.processTemplate(scriptId, groupId, embedId);
+                return new MethodResult(preCode + functionCode + code);
+            }
         }
+        throw (new MethodError('not found', 404));
     }
 
-    static processTemplate(script_id, group_id, embed_id) {
-        let function_code: string = FS.readFileSync('./content/pipe_functions.js', { encoding: 'utf-8' });
-        const dataUrl = script_id + '/' + group_id + '/' + embed_id;
-        function_code = function_code.replace(/\$SCRIPTURL\$/g, `serve/${dataUrl}`);
-        function_code = function_code.replace(/\$LOGURL\$/g, `log/${dataUrl}`);
-        function_code = function_code.replace(/\$RESULTURL\$/g, `results/${dataUrl}/`);
-        function_code = function_code.replace(/\$SERVERURL\$/g, `https://skribo.herokuapp.com/`);
-        function_code = function_code.replace(/\$SYNCURL\$/g, `sync/${group_id}/`);
-        function_code = function_code.replace(/\$SKRIBODATA\$/g, `'` + JSON.stringify({
-            'group_id': group_id,
-            'base_url': 'https://skribo.herokuapp.com'
-        }) + `'`);
-
-        return function_code;
-    }
-
-    static generateVariables(variables: any[]) {
+    private static generateVariables(variables: any[]) {
         return [
             'var SkriboEnv =  {',
             ...variables.map((item) => {
@@ -87,32 +55,58 @@ export class Serve {
                 }
 
             }),
-            '};'
+            '};',
         ].join('\n');
     }
-    
-    @Method(Verbs.Get, '/serve/:script_id/:group_id/:embed_id')
-    public static async get(@Param('script_id') script_id: string, @Param("group_id") group_id: string, @Param('embed_id') embed_id: string): Promise<MethodResult<string>> {
-        try {
-            const codeResult = await ScriptModel.query(new DataQuery(ScriptModel).filter({ 'ScriptId': script_id }));
-            if (codeResult.length > 0) {
-                let code = codeResult[0].Code;
-                const InstanceScript = (await new DataQuery(EmbedModel).filter({ GroupId: group_id, ScriptId: script_id, EmbedId: embed_id }).run());
-                if (InstanceScript.length > 0) {
-                    let variables = InstanceScript[0].Variables || [];
-                    if (typeof variables === 'string') {
-                        variables = JSON.parse(variables);
-                    }
-                    let preCode = this.generateVariables(variables);
-                    let function_code: string = this.processTemplate(script_id, group_id, embed_id);
-                    console.log('complete script', preCode)
-                    return new MethodResult(preCode + function_code + code);
-                }
+
+    private static timespanToRange(timespan) {
+        // 'TODAY', 'YESTERDAY', 'LAST_7_DAYS', 'THIS_WEEK_SUN_TODAY',
+        // 'THIS_WEEK_MON_TODAY', 'LAST_WEEK', 'LAST_14_DAYS', 'LAST_30_DAYS',
+        // 'LAST_WEEK', 'LAST_BUSINESS_WEEK', 'LAST_WEEK_SUN_SAT', 'THIS_MONTH', 'LAST_MONTH', 'ALL_TIME'
+        switch (timespan) {
+            case 'TODAY': {
+                const dateString = new Date().toISOString().split('T')[0];
+                return { start: dateString, end: dateString };
             }
-            throw (new MethodError('not found', 404));
-        }
-        catch (error) {
-            console.error(error);
+
+            case 'YESTERDAY': {
+                const date = new Date();
+                date.setDate(date.getDate() - 1);
+                const dateString = date.toISOString().split('T')[0];
+                return { start: dateString, end: new Date().toISOString().split('T')[0] };
+            }
+
+            case 'LAST_7_DAYS': {
+                const date = new Date();
+                date.setDate(date.getDate() - 7);
+                const dateString = date.toISOString().split('T')[0];
+                return { start: dateString, end: new Date().toISOString().split('T')[0] };
+            }
+
+            case 'LAST_MONTH':
+                {
+                    const date = new Date();
+                    date.setDate(date.getDate() - 30);
+                    const dateString = date.toISOString().split('T')[0];
+                    return { start: dateString, end: new Date().toISOString().split('T')[0] };
+                }
         }
     }
+
+    private static processTemplate(scriptId: string, groupId: string, embedId: string): string {
+        let functionCode: string = FS.readFileSync('./content/pipe_functions.js', { encoding: 'utf-8' });
+        const dataUrl = scriptId + '/' + groupId + '/' + embedId;
+        functionCode = functionCode.replace(/\$SCRIPTURL\$/g, `serve/${dataUrl}`);
+        functionCode = functionCode.replace(/\$LOGURL\$/g, `log/${dataUrl}`);
+        functionCode = functionCode.replace(/\$RESULTURL\$/g, `results/${dataUrl}/`);
+        functionCode = functionCode.replace(/\$SERVERURL\$/g, `https://skribo.herokuapp.com/`);
+        functionCode = functionCode.replace(/\$SYNCURL\$/g, `sync/${groupId}/`);
+        functionCode = functionCode.replace(/\$SKRIBODATA\$/g, `'` + JSON.stringify({
+            base_url: 'https://skribo.herokuapp.com',
+            group_id: groupId,
+        }) + `'`);
+
+        return functionCode;
+    }
+
 }
