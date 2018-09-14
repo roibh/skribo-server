3c8efb86b9aa85d8a6f978d9b03196d4dad79460
Roi ben haim <roi.benhaim@gmail.com>
2018-09-14 21:56:35 +0300

    no message

--- db/hash.ts
+++ db/hash.ts
@@ -1,8 +1,8 @@
 export function hashCode(str) {
-    var hash = 0;
-    if (str.length == 0) return hash;
+    let hash = 0;
+    if (str.length === 0) { return hash; }
     for (let i = 0; i < str.length; i++) {
-        let char = str.charCodeAt(i);
+        const char = str.charCodeAt(i);
         hash = ((hash << 5) - hash) + char;
         hash = hash & hash; // Convert to 32bit integer
     }
